2c10bf486b0ab929f63b7cfdf29013cae5b42f37
dana367 <dana.larina1@gmail.com>
2025-01-23 11:18:09 +0000

    Removed unnecessary lines in main.py

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 app = FastAPI()
-# app.include_router(auth.router)
-# app.include_router(black_scholes_calculation.router)
-
 
 origins = ["http://localhost:3000"]
 
@@ -25,15 +22,6 @@ app.add_middleware(
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
-
-
-# @app.get("/")
-# async def root():
-#     return {
-#         "message": "Welcome to Black-Scholes Calculator API",
-#         "docs": "/docs",
-#         "version": app.version,
-#     }
 
 
 @app.get("/", status_code=200)
